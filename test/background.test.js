071b4cc8639cb79d786fb4e68ac038e487b8a284
jh <partiallyconcerned@gmail.com>
2025-06-05 13:32:49 -0400

    Add CORS response header spoofing

--- test/background.test.js
+++ test/background.test.js
@@ -10,12 +10,39 @@ describe('delay function', () => {
 });
 
 describe('fetchWithCookie', () => {
-  test('sends cookie header when provided', async () => {
+  test('uses fetch with credentials', async () => {
     global.fetch = jest.fn().mockResolvedValue({status: 200});
-    await fetchWithCookie('http://example.com', 'foo=bar');
+    await fetchWithCookie('http://example.com');
     expect(global.fetch).toHaveBeenCalledWith('http://example.com', {
-      credentials: 'include',
-      headers: { Cookie: 'foo=bar' }
+      credentials: 'include'
     });
+  });
+
+  test('uses XMLHttpRequest when available', async () => {
+    const open = jest.fn();
+    const send = jest.fn(function() {
+      this.status = 200;
+      this.statusText = 'OK';
+      this.response = 'data';
+      if (this.onload) this.onload();
+    });
+    function FakeXHR() {
+      this.open = open;
+      this.send = send;
+    }
+    const xhrInstance = new FakeXHR();
+    global.XMLHttpRequest = jest.fn(() => xhrInstance);
+    global.fetch = jest.fn();
+
+    const res = await fetchWithCookie('http://example.com/resource');
+
+    expect(global.XMLHttpRequest).toHaveBeenCalled();
+    expect(open).toHaveBeenCalledWith('GET', 'http://example.com/resource', true);
+    expect(xhrInstance.responseType).toBe('blob');
+    expect(xhrInstance.withCredentials).toBe(true);
+    expect(global.fetch).not.toHaveBeenCalled();
+    expect(res.status).toBe(200);
+
+    delete global.XMLHttpRequest;
   });
 });
